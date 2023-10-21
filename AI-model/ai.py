aaff61801deed43eff79fed4ed6e65dd69a214ff
“[Karan <[karankumar18.06.2004@gmail.com]>
2023-10-21 19:43:03 +0530

    push2

--- AI-model/ai.py
+++ AI-model/ai.py
@@ -5,3 +5,29 @@ Created on Sat Oct 21 17:51:19 2023
 @author: Rajiv Arora
 """
 
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
+
+
+
+
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
